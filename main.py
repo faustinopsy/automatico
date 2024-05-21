0713ec55667b7f07228a142ce422159d9d8cfb25
faustinopsy <rodrigohipnose@gmail.com>
2024-05-20 22:26:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ import time
 import os
 from plyer import notification
 import winsound
-from playsound import playsound
+from gtts import gTTS
 
 def alerta_medicamento(nome_medicamento):
     notification.notify(
         title='Hora do Medicamento',
-        message=f'É hora de tomar o seu medicamento: {nome_medicamento}',
+        message=f'É hora : {nome_medicamento}',
         timeout=10
     )
-    winsound.Beep(1000, 1500)
-    playsound('som.mp3')
-    print(f'Notificação enviada: {nome_medicamento}')
+    #winsound.Beep(1000, 1500)
+    intro=f'Hora de tomar :{nome_medicamento}'
+    language='pt'
+    voz=gTTS(intro,lang=language,slow=False)
+    voz.save("bem_vindo.mp3")
+    os.system("start bem_vindo.mp3")
+    print(f'Notificação: {nome_medicamento}')
 
-
+#alerta_medicamento("teste de som, aqui")
 def salvar_medicamentos(medicamentos, arquivo='medicamentos.txt'):
     with open(arquivo, 'a') as f:
         for medicamento, horario in medicamentos.items():
             f.write(f'{medicamento},{horario}\n')
 
+#salvar_medicamentos({"para coluna": "20:55"})
 def carregar_medicamentos(arquivo='medicamentos.txt'):
     medicamentos = {}
     if os.path.exists(arquivo):
@@ -30,35 +35,38 @@ def carregar_medicamentos(arquivo='medicamentos.txt'):
                 medicamentos[medicamento] = horario
     return medicamentos
 
+#print(carregar_medicamentos())
 
 def adicionar_medicamentos():
     medicamentos = {}
     while True:
         medicamento = input('Digite o nome do medicamento: ')
-        horario = input('Digite o horário para tomar o medicamento (HH:MM): ')
+        horario = input('Digite o horário   (HH:MM): ')
         medicamentos[medicamento] = horario
-        continuar = input('Deseja adicionar mais medicamentos? (s/n): ')
+        continuar = input('Deseja adicionar mais ? (s/n): ')
         if continuar.lower() != 's':
             break
     return medicamentos
 
 
 
+
 if not os.path.exists('medicamentos.txt'):
-    print('Arquivo de medicamentos não encontrado. Vamos adicionar alguns medicamentos.')
+    print('Arquivo de medicamentos não encontrado.')
     medicamentos = adicionar_medicamentos()
     salvar_medicamentos(medicamentos)
 else:
     medicamentos = carregar_medicamentos()
 
+
+
+
 for medicamento, horario in medicamentos.items():
-    schedule.every().day.at(horario).do(alerta_medicamento, nome_medicamento=medicamento)
+    schedule.every().day.at(horario).do(
+        alerta_medicamento, nome_medicamento=medicamento
+        )
 
 print('Agendador de medicamentos iniciado...')
 while True:
     schedule.run_pending()
     time.sleep(1)
-
-
-
-
